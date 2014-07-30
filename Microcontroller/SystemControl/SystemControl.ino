5f71249ab1ac940b17fd331f80d6ef2de96e7669
Bruce McAlister <bmcalister@umass.edu>
2014-07-29 23:39:58 -0400

    Addition of sketch Arduino code file to the microcontroller directory

---
+++ Microcontroller/SystemControl/SystemControl.ino
@@ -0,0 +1,4 @@
+//This file runs on the microcontroller that monitors and controls the power for the Flying Fish 
+//version of the Harvard Forest remote data collection tram project.
+//This microcontroller monitors the health of the system and controls when the computers and radios
+//are started, run and shutdown.
